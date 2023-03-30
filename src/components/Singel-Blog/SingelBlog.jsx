3b5e97a3756d87f1b6b98820450e8f1ce02c0544
unknown <mdtanvir7462@gmail.com>
2023-03-31 00:39:04 +0600

    blog data dynamicaly addeded

--- src/components/Singel-Blog/SingelBlog.jsx
+++ src/components/Singel-Blog/SingelBlog.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 
 const SingelBlog = (props) => {
-   console.log(props.user)
+   const { id, Authorname, BlogTitle, Publishdate, Readtime, Authorimg, BlogCoverImg } = props.user
    return (
 
       <div class="max-w-2xl h- bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
 
-         <img class="rounded-t-lg" src={props.user.BlogCoverImg} alt="" />
+         <img class="rounded-t-lg" src={BlogCoverImg} alt="" />
 
          <div class="p-5">
+            <p className='flex gap-4'>
+               <img className="w-10 h-10 rounded-full" src={Authorimg} alt="" />
+               <span>  <p className='font-bold'>{Authorname}</p>
+                  <p>{Publishdate}</p></span>
 
-            <img className="w-10 h-10 rounded-full" src="../../../public/users/user (1).png" alt="" />
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.user.BlogTitle}</h5>
+            </p>
+
+            <h5 class="mb-2 text-base md:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{BlogTitle}</h5>
 
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
-           
+
          </div>
       </div>
 
