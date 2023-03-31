c178ce0961100d0ade2b58e95f1e3758338a03d8
unknown <mdtanvir7462@gmail.com>
2023-03-31 13:07:50 +0600

    blog cards finished

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,20 +3,13 @@ import React from 'react';
 const Nav = () => {
    return (
       <div>
-         <nav className="bg-pink-200 mb-2 border-gray-200 dark:bg-gray-900 rounded-md">
+         <nav className="bg-pink-200 mt-1 border-gray-200 dark:bg-gray-900 rounded-md">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <p className="flex items-center">
       <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
-      <span className="self-center text-2xl font-semibold whitespace-nowrap text-black-900">Knowledge Cafe</span>
+      <span className="self-center  md:text-2xl font-semibold whitespace-nowrap text-black-900">Knowledge Cafe</span>
   </p>
-  <div>
-    <ul className='flex gap-14 text-black font-bold ' >
-      <li className=' hover:text-pink-800'>Home</li>
-      <li className=' hover:text-pink-800'>Menu</li>
-      <li className=' hover:text-pink-800'>Contact</li>
-    </ul>
-
-  </div>
+ 
   <div className="flex items-center md:order-2">
       <button type="button" className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
         
--- src/components/Singel-Blog/SingelBlog.jsx
+++ src/components/Singel-Blog/SingelBlog.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
-
 const SingelBlog = (props) => {
    const { id, Authorname, BlogTitle, Publishdate, Readtime, Authorimg, BlogCoverImg } = props.user
    return (
 
-      <div class="max-w-2xl h- bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div class="max-w-2xl mb-4 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
 
          <img class="rounded-t-lg" src={BlogCoverImg} alt="" />
 
          <div class="p-5">
-            <p className='flex gap-4'>
+            <div className='flex justify-between'>
+            <p className='flex gap-4 items-cente mb-5'>
                <img className="w-10 h-10 rounded-full" src={Authorimg} alt="" />
                <span>  <p className='font-bold'>{Authorname}</p>
-                  <p>{Publishdate}</p></span>
+                  <p className='text-xs font-semibold text-slate-700'>{Publishdate}</p></span>
 
             </p>
+            <p className='flex items-center'>
+            <span className='text-slate-700 mr-2'>{Readtime} min read</span> 
+              
+               <button type="button" class="text-gray-900 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-md text-sm  dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 "> <img  className='w-5 h-4 ' src="../../../public/bookmark-regular.svg" alt="" /></button>
+            </p>
+           
 
-            <h5 class="mb-2 text-base md:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{BlogTitle}</h5>
 
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
+            </div>
+            <h5 class="mb-2 text-base md:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{BlogTitle}  
+            </h5>
+
+            <button type="button" class="focus:outline-none text-blue-800  font-medium rounded-lg text-sm  px-1  hover:bg-purple-200  py-2.5 mb-2 "> <span className='underline font-bold'>Mark as read</span></button>
 
          </div>
       </div>
